12e7ca8a6d0a987cd5206a9412ab0c33a093591a
daanh432 <daan@daanhendriks.nl>
2019-05-21 11:32:15 +0200

    Updated courses table with description and integer difficulty

---
+++ database/migrations/2019_05_21_092047_update_courses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->string('description', 2048);
+            $table->unsignedTinyInteger('difficulty')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->dropColumn(['description']);
+            $table->string('difficulty')->change();
+        });
+    }
+}
