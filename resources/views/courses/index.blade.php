941d6aa1f5c3a84cf394028996b9ddbff29f728b
Unknown <pimmetos@hotmail.com>
2019-05-21 11:37:27 +0200

    Courses index page with integer star based difficulty levels

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -1,1 +1,32 @@
-{{ dump($courses) }}
+@extends('layouts.app')
+
+@section('title', 'Courses')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="courseGridContainer my-5 ">
+                @foreach($courses as $course)
+                    <div class="gridItem p-3 br-20 position-relative">
+                        <h1>{{ $course->name }}</h1>
+                        <div class="difficulty position-absolute">
+                            @for($i =0; $i <= $course->difficulty; $i++)
+                                <i class="fas fa-star"></i>
+                            @endfor
+                        </div>
+                        <p>{{ $course->description }}</p>
+                        <p class="position-absolute duration">Duration: {{ $course->duration }}. </p>
+                        <a class="btn btn-info w-25 br-20 position-absolute">
+                            @if($course->price >= 1)
+                                Unlock for {{ $course->price }} points
+                            @else
+                                Start
+                            @endif
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+
+@endsection
