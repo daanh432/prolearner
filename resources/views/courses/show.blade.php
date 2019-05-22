9c560e9397a57284d1bd47cd32223cdb541caab6
Stijn Lingmont <stijnlingmont@hotmail.com>
2019-05-22 15:35:28 +0200

    Course show header text centered responsive

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -6,9 +6,9 @@
     <div class="container-fluid">
         <div id="courseHeaderImage" class="row parallax-row" data-parallax="scroll" data-image-src="{{ asset('/assets/img/parallaxBackground.jpg') }}">
             <div class="container">
-                <div class="row">
-                    <div class="col-md-10 mx-auto">
-                        <div class="text-center">
+                <div class="row h-100">
+                    <div class="col-md-10 mx-auto h-100">
+                        <div id="courseName">
                             <h1 class="secondaryText">{{ $course->name }}</h1>
                         </div>
                     </div>
