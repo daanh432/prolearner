5c13ae7e07577f62e643f826bf6aac4e9ffbf126
daanh432 <daan@daanhendriks.nl>
2019-05-22 13:04:57 +0200

    Switched default app language to English and updated course show page

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -3,38 +3,44 @@
 @section('title', 'Courses')
 
 @section('content')
-    <div class="row-full parallax-row" data-parallax="scroll" data-image-src="{{ asset('/assets/img/parallaxBackground.jpg') }}">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h1 class="secondaryText">{{ $course->name }}</h1>
+    <div class="container-fluid">
+        <div id="" class="row parallax-row" data-parallax="scroll" data-image-src="{{ asset('/assets/img/parallaxBackground.jpg') }}">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10 mx-auto">
+                        <div class="text-center">
+                            <h1 class="secondaryText">{{ $course->name }}</h1>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-md-12">
-            <div id="accordion" class="w-100">
-                @foreach($course->Chapters() as $chapter)
-                    <div class="card">
-                        <a class="lessonHeader card-link" data-toggle="collapse" href="#collapseOne">
-                            <div class="card-header primaryText">
-                                {{ $chapter->name }}
-                            </div>
-                        </a>
-                        <div id="collapseOne" class="collapse show" data-parent="#accordion">
-                            <div class="card-body p-0">
-                                <ul class="pl-0 mb-0 lessons">
-                                    @foreach ($chapter->Lessons() as $lesson)
-                                        <a href="" class="text-decoration-none">
-                                            <li class="list-style-type-none p-3 primaryText">{{ $lesson->name }}</li>
-                                        </a>
-                                    @endforeach
-                                </ul>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div id="accordion" class="w-100">
+                    @foreach($course->Chapters() as $chapter)
+                        <div class="card">
+                            <a class="lessonHeader card-link" data-toggle="collapse" href="#collapseOne">
+                                <div class="card-header primaryText">
+                                    {{ $chapter->name }}
+                                </div>
+                            </a>
+                            <div id="collapseOne" class="collapse show" data-parent="#accordion">
+                                <div class="card-body p-0">
+                                    <ul class="pl-0 mb-0 lessons">
+                                        @foreach ($chapter->Lessons() as $lesson)
+                                            <a href="" class="text-decoration-none">
+                                                <li class="list-style-type-none p-3 primaryText">{{ $lesson->name }}</li>
+                                            </a>
+                                        @endforeach
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                @endforeach
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
