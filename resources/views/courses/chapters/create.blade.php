e174df7969b6e586ed642771326fd46fcdc8ac84
Unknown <pimmetos@hotmail.com>
2019-05-29 09:38:37 +0200

    Create chapter design changes

--- resources/views/courses/chapters/create.blade.php
+++ resources/views/courses/chapters/create.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <form action="{{ route('courses.chapters.store', [ 'course' => $course->id]) }}" method="post" id="storeChapter" enctype="multipart/form-data" class="needs-validation col-md-6 mx-auto bg-white p-5 mt-5 rounded" novalidate>
+            <form action="{{ route('courses.chapters.store', [ 'course' => $course->id]) }}" method="post" id="storeChapter" enctype="multipart/form-data" class="needs-validation col-md-6 mx-auto containerBackground p-5 mt-5 br-20 secondaryText" novalidate>
                 @csrf
                 <h1>Create chapter</h1>
                 <div class="form-group">
@@ -13,7 +13,15 @@
                     <div class="valid-feedback">Valid.</div>
                     <div class="invalid-feedback">Please fill out this field.</div>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+
+                <div class="form-group row">
+                    <div class="col-md-6 text-center">
+                        <a href="{{ URL::previous() }}" class="btn btn-secondary w-75 br-20">{{ __('pages.goBack') }}</a>
+                    </div>
+                    <div class="col-md-6 mt-2 mt-md-0 text-center">
+                        <button class="btn btn-primary w-75 br-20" type="submit">{{ __('pages.submit') }}</button>
+                    </div>
+                </div>
             </form>
         </div>
     </div>
