724a7f1014d325ce8cd77a0856c3cc5a34ec2583
daanh432 <daan@daanhendriks.nl>
2019-06-12 17:27:51 +0200

    Favicon for website

---
+++ resources/views/components/favicon.blade.php
@@ -0,0 +1,12 @@
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/favicon/apple-touch-icon.png') }}?v=9BPpnpy2En">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/favicon/favicon-32x32.png') }}?v=9BPpnpy2En">
+<link rel="icon" type="image/png" sizes="192x192" href="{{ asset('assets/favicon/android-chrome-192x192.png') }}?v=9BPpnpy2En">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/favicon/favicon-16x16.png') }}?v=9BPpnpy2En">
+<link rel="manifest" href="{{ asset('assets/favicon/site.webmanifest') }}?v=9BPpnpy2En">
+<link rel="mask-icon" href="{{ asset('assets/favicon/safari-pinned-tab.svg') }}?v=9BPpnpy2En" color="#5bbad5">
+<link rel="shortcut icon" href="{{ asset('assets/favicon/favicon.ico') }}?v=9BPpnpy2En">
+<meta name="apple-mobile-web-app-title" content="ProLearner">
+<meta name="application-name" content="ProLearner">
+<meta name="msapplication-TileColor" content="#00aba9">
+<meta name="msapplication-config" content="{{ asset('assets/favicon/browserconfig.xml') }}?v=9BPpnpy2En">
+<meta name="theme-color" content="#ffffff">
