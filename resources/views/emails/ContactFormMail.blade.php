d88792ecea70453ab9e0db51b5d20fa50d50b693
daanh432 <daan@daanhendriks.nl>
2019-06-19 09:57:01 +0200

    Contact form mail fix

--- resources/views/emails/ContactFormMail.blade.php
+++ resources/views/emails/ContactFormMail.blade.php
@@ -1,18 +1,19 @@
 @component('mail::message')
-    # Contact form submission
+# Contact form submission
 
-    You received a message on https://prolearner.daanhendriks.nl/.
+You received a message on https://prolearner.daanhendriks.nl/.
 
-    First name: {{ $submission->name }}<br>
-    Email address: {{ $submission->email }}<br>
-    Subject: {{ $submission->subject }}<br>
-    Message:<br>
-    {{ $submission->message }}
+First name: {{ $submission->firstName }}<br>
+Last name: {{ $submission->lastName }}<br>
+Email address: {{ $submission->emailAddress }}<br>
+Subject: {{ $submission->subject }}<br>
+Message:<br>
+{{ $submission->message }}
 
-    @component('mail::button', ['url' => 'mailto:' . $submission->email ])
-        Respond to message
-    @endcomponent
+@component('mail::button', ['url' => 'mailto:' . $submission->emailAddress ])
+Respond to message
+@endcomponent
 
-    Kind regards,<br>
-    {{ config('app.name') }}
+Kind regards,<br>
+{{ config('app.name') }}
 @endcomponent
